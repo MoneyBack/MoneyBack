b6c3718c4e4b239d2c511206052cf09abd28e1e4
guangxing.chen <chengx168000@outlook.com>
2013-09-04 18:13:10 +0800

    implement home action logic

--- mb/actions.py
+++ mb/actions.py
@@ -7,6 +7,7 @@ Created on Aug 30, 2013
 from mb.config import DB_TABLE_ELECTRIC_PURCHASER
 from mb.db import MBDB
 from mb.cache import MBCache
+from mb.config import CACHE_PREFIX_ALLIANCE, CACHE_PREFIX_CATRGORY, CACHE_PREFIX_SUB_CATEGORY
 
 class HomeAction():
     
@@ -14,10 +15,22 @@ class HomeAction():
         homeData = {}
         #先从缓存取相关信息，如果没有则从数据库取信息并放入缓存
         allSiteInfos =  MBDB.select(DB_TABLE_ELECTRIC_PURCHASER)
+        allCatSet = set()
+        catSiteMap = dict()
         for siteInfo in allSiteInfos:
-            pass
+            merchantId = siteInfo.merchant_id
+            merchantCatId = siteInfo.merchant_cat_id
+            allCatSet.add(merchantId)
+            catSites = catSiteMap.get(merchantCatId)
+            if catSites:
+                catSites.append(merchantId)
+            else:
+                catSiteMap[merchantCatId] = [merchantId]
+            MBCache.set('%s%s' % (CACHE_PREFIX_ALLIANCE, merchantId), siteInfo)
+        MBCache.set(CACHE_PREFIX_CATRGORY, list(allCatSet))
+        for (k, v) in catSiteMap.items():
+            MBCache.set('%s%s' % (CACHE_PREFIX_SUB_CATEGORY, k), v)
         return homeData
         
 
 homeAction = HomeAction()
-
