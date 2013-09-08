1090d2db134ea120cce4e2c5f80917cf8d0941d8
root <root@localhost.(none)>
2013-09-08 03:18:24 -0700

    fix the load module error

--- main_test.py
+++ main_test.py
@@ -4,14 +4,22 @@ Created on Aug 29, 2013
 
 @author: Carl, Aaron
 '''
+import os
+import sys
+
+curdir = os.path.dirname(__file__)
+sys.path.insert(0, curdir)
 
 import web
-from mbtest import *
+from mbtest.testDb import TestDB
 
 urls = (
     '/', 'index',
     '/test_db', 'TestDB'
 )
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
 
 class index:
     def GET(self):
