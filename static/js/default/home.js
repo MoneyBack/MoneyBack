5691407b3219968e866b25da6375ba7d626f874c
guangxing.chen <chengx168000@outlook.com>
2013-09-12 01:23:40 +0800

    add some home stuff

--- static/js/default/home.js
+++ static/js/default/home.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	var sites = homeData.sites;
 	$('.merchant').click(function(){
-		window.open(sites[this.id].click_url, '_blank');
-		$.ajax({
-			type: "POST",
-			url: "/openSite",
-			data: {siteId:this.id},
-			success: function(data) {}
-		});
+		if (!!this.id) {
+			window.open(sites[this.id].click_url, '_blank');
+			$.ajax({
+				type: "POST",
+				url: "/openSite",
+				data: {siteId:this.id},
+				success: function(data) {}
+			});
+		}
 	});
 });
