0e80582a569f2d6cfbc44778284b9bea8a1c846b
guangxing.chen <chengx168000@outlook.com>
2013-09-23 18:11:43 +0800

    adding home stuff

--- static/js/default/home.js
+++ static/js/default/home.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 	var sites = homeData.sites;
+	$('.merchant_s').click(function(){
+		if (!!this.id) {
+			// 设置站点数据
+			var merchantImg = $('#current').find('img');
+			merchantImg.unbind();
+			merchantImg.attr('src', '/static/img/default/sites/' + sites[this.id].pretty_logo + '.png');
+			merchantImg.parent().attr('id', this.id);
+			var sInfo = merchantImg.parent().siblings('.s_info');
+			sInfo.removeClass('none');
+			var sInfoLeft = sInfo.find('.s_info_left');
+			sInfoLeft.attr('title', sites[this.id].rebate_info);
+			sInfoLeft.text(sites[this.id].rebate_info);
+			var fInfo = merchantImg.parent().siblings('.f_info');
+			fInfo.removeClass('none');
+			mbMark = fInfo.find('.mb_mark');
+			mbPrefer = fInfo.find('.mb_prefer');
+			if (sites[this.id].rebate != 1) {
+				mbMark.addClass('invisible');
+			} else {
+				mbMark.removeClass('invisible');
+			}
+			if (sites[this.id].type != 3) {
+				mbPrefer.addClass('invisible');
+			} else {
+				mbPrefer.removeClass('invisible');
+			}
+		}
+		$('#backdrop').addClass('hide');
+		$('#homeContainer').removeClass('blur');
+		$('#all_sites').addClass('hide');
+	});
 	$('.merchant').click(function(){
 		if (!!this.id) {
 			window.open(sites[this.id].click_url, '_blank');
@@ -10,7 +41,8 @@ $(document).ready(function(){
 				success: function(data) {}
 			});
 		} else {
-			// 添加站点
+			// 打开添加站点面板
+			$(this).attr('id', 'current');
 			$('#backdrop').removeClass('hide');
 			$('#homeContainer').addClass('blur');
 			$('#all_sites').removeClass('hide');
@@ -18,21 +50,6 @@ $(document).ready(function(){
 				var siteImage = $(this).children()[0];
 				$(siteImage).attr('src', sites[this.id].logo_url);
 			});
-			// 获得站点数据
-			// ...
-			// 设置站点数据
-			/*
-			var merchantImg = $(this).find('img');
-			merchantImg.unbind();
-			merchantImg.attr('src', '/static/img/default/sites/yhd.png');
-			merchantImg.parent().attr('id', '1104');
-			var sInfo = merchantImg.parent().siblings('.s_info');
-			sInfo.removeClass('none');
-			var sInfoLeft = sInfo.find('.s_info_left');
-			sInfoLeft.attr('title', 'title-test');
-			sInfoLeft.text('value-test');
-			var fInfo = merchantImg.parent().siblings('.f_info');
-			fInfo.removeClass('none');*/
 		}
 	});
 	$('.s_info .delete').click(function(){
